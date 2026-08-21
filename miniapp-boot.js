f6042d79da9fabc4785093f119f02d2f7ef1fc3e
extazzz1997 <extazzz1997@gmail.com>
2026-08-22 00:41:47 +0300

    feat: restore 500 and 5000 Stars pricing

--- miniapp-boot.js
+++ miniapp-boot.js
@@ -2,7 +2,7 @@
   "use strict";
 
   var configVersion = Date.now();
-  var assetVersion = "20260821-outcome-evidence-1";
+  var assetVersion = "20260822-price-v3-1";
   var timer;
   var failed = false;
 
